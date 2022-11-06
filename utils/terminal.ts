f294a7256e04e7bd111adde9296681790402eb0e
João Corsini <jf.corsini@gmail.com>
2022-11-06 11:55:21 +0200

    Improve coloring of terminal

--- utils/terminal.ts
+++ utils/terminal.ts
@@ -4,7 +4,7 @@ const warning = "#D9D9AC"
 const danger = "#C5947C"
 
 // <span style="color:blue">
-const colorValue = (value: number, dangerMin: number | null, warningMin: number | null, warningMax: number, dangerMax:number) => {
+const getOpenSpan = (value: number, dangerMin: number | null, warningMin: number | null, warningMax: number, dangerMax:number) => {
   let color="white";
   if (dangerMin && value <= dangerMin || value >= dangerMax) {
     color = danger;
@@ -12,7 +12,7 @@ const colorValue = (value: number, dangerMin: number | null, warningMin: number 
     color = warning;
   }
 
-  return `<span style="color:${color}">${value}</span>`;
+  return `<span style="color:${color}">`;
 }
 
 export const getDailyText = (mock: Mock, dayAddition: number) => {
@@ -21,12 +21,12 @@ export const getDailyText = (mock: Mock, dayAddition: number) => {
   const weekends = Math.floor((dayAddition) / 5)
   formattedDate.setDate(firstDate.getDate() + dayAddition + weekends*2);
   const result = `Date: ${formattedDate.toLocaleDateString()}:
-  - Sleep: ${colorValue(mock.value.sleep, 3, 5, 11, 13)} hours
-  - Heart Rate Variability: ${colorValue(mock.value.hrv, 10, 28.5, 103.75, 122.5)} ms
-  - Temperature: ${colorValue(mock.value.temp, 35, 35.5, 38, 40)} °C
-  - Activity: ${colorValue(mock.value.activity, 5, 10, 120, 180)} min
-  - Respiratory Rate: ${colorValue(mock.value.respiratory, 10, 12, 22, 24)} breaths per minute
-  - Work hours logged: ${colorValue(mock.value.workHours, null, null, 10, 12)} hours
+  - ${getOpenSpan(mock.value.sleep, 3, 5, 11, 13)} Sleep: ${mock.value.sleep} hours</span>
+  - ${getOpenSpan(mock.value.hrv, 10, 28.5, 103.75, 122.5)} Heart Rate Variability: ${mock.value.hrv} ms</span>
+  - ${getOpenSpan(mock.value.temp, 35, 35.5, 38, 40)} Temperature: ${mock.value.temp} °C</span>
+  - ${getOpenSpan(mock.value.activity, 5, 10, 120, 180)} Activity: ${mock.value.activity} min</span>
+  - ${getOpenSpan(mock.value.respiratory, 10, 12, 22, 24)} Respiratory Rate: ${mock.value.respiratory} breaths per minute</span>
+  - ${getOpenSpan(mock.value.workHours, null, null, 10, 12)} Work hours logged: ${mock.value.workHours} hours</span>
   `
 
   if (mock.action) {
