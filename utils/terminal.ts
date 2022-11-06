1311b189c1dec4feb89d3304db7bb194f986b8d0
João Corsini <jf.corsini@gmail.com>
2022-11-06 11:49:39 +0200

    feat: Add some colors

--- utils/terminal.ts
+++ utils/terminal.ts
@@ -1,23 +1,37 @@
 import { Mock, mockData } from "./mock";
 
+const warning = "#D9D9AC"
+const danger = "#C5947C"
+
 // <span style="color:blue">
+const colorValue = (value: number, dangerMin: number | null, warningMin: number | null, warningMax: number, dangerMax:number) => {
+  let color="white";
+  if (dangerMin && value <= dangerMin || value >= dangerMax) {
+    color = danger;
+  } else if ((warningMin && value < warningMin) || value > warningMax) {
+    color = warning;
+  }
+
+  return `<span style="color:${color}">${value}</span>`;
+}
+
 export const getDailyText = (mock: Mock, dayAddition: number) => {
   const formattedDate = new Date(mock.date)
   const firstDate = new Date(mockData[0].date)
   const weekends = Math.floor((dayAddition) / 5)
   formattedDate.setDate(firstDate.getDate() + dayAddition + weekends*2);
   const result = `Date: ${formattedDate.toLocaleDateString()}:
-  - Sleep: ${mock.value.sleep} hours
-  - Heart Rate Variability: ${mock.value.hrv} ms
-  - Temperature: ${mock.value.temp} °C
-  - Activity: ${mock.value.activity} min
-  - Respiratory Rate: ${mock.value.respiratory} breaths per minute
-  - Work hours logged: ${mock.value.workHours} hours
+  - Sleep: ${colorValue(mock.value.sleep, 3, 5, 11, 13)} hours
+  - Heart Rate Variability: ${colorValue(mock.value.hrv, 10, 28.5, 103.75, 122.5)} ms
+  - Temperature: ${colorValue(mock.value.temp, 35, 35.5, 38, 40)} °C
+  - Activity: ${colorValue(mock.value.activity, 5, 10, 120, 180)} min
+  - Respiratory Rate: ${colorValue(mock.value.respiratory, 10, 12, 22, 24)} breaths per minute
+  - Work hours logged: ${colorValue(mock.value.workHours, null, null, 10, 12)} hours
   `
 
   if (mock.action) {
     return result + `
-    <span style="color:#c5947c">
+    <span style="color:${danger}">
     ....................
     Flare detected.
     Action: ${mock.action}
